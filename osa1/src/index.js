0a774baa64593d591ab5ed8da9cb05b2ef3d4991
ikonenjoel <drakening@gmail.com>
2020-04-10 08:24:55 +0300

    fixed a fucked up commit.

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,41 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
 const Header = (props) => {
-  return (
-    <>
-      {props.course}
-    </>
+  return(
+    <p>{props.course}</p>
   )
 }
 
 const Content = (props) => {
-  return (
-    <>
-
-    </>
+  console.log(props)
+  return(
+    <div>
+      <Part part1={part1.name} exercises1={part1.exercises}/>
+      <Part part2={part2.name} exercises2={part2.exercises}/>
+      <Part part3={part3.name} exercises3={part3.exercises}/>
+    </div>
   )
 }
 
-const Total = () => {
-
+const Total = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <p>{props.part1}</p>
+      <p>{props.part2}</p>
+      <p>{props.part3}</p>
+    </div>
+  )
 }
 
-const App = (props) => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of react'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.part1} {props.exercises1} {props.part2} {props.exercises2} {props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
 
-
-return (
-  <div>
-    <Header course={course} />
-    <Content />
-  </div>
+  return (
+    <div>
+      <Header course={course} />
+      <Content Part={part1} part2={part2} part3={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+    </div>
   )
 }
 
